5039dcf3fe4caf006082491af17fdc42b569592d
Icaro Asas Virtuais <39817766+asasvirtuais@users.noreply.github.com>
2024-08-25 04:51:38 +0000

    + provider callback endpoint

---
+++ app/oauth/[provider]/callback/route.ts
@@ -0,0 +1,45 @@
+import { handleNextRouteAsync, returnRedirect } from '@/app/next/route/utils'
+import { tokenUrl, clientId, clientSecret, redirectUri } from '@/app/oauth/util'
+import { decodeState } from '@/app/oauth/state'
+import { insert } from '@/app/oauth/credentials'
+import { TokenData } from '@/app/oauth/types'
+
+export const dynamic = 'force-dynamic'
+
+const exchangeCodeForToken = async (provider: string, code: string): Promise<TokenData> => {
+    const response = await fetch(tokenUrl(provider) as string, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams({
+            client_id: clientId(provider) as string,
+            client_secret: clientSecret(provider) as string,
+            code,
+            grant_type: 'authorization_code',
+            redirect_uri: redirectUri()
+        })
+    })
+
+    const result = await response.json()
+
+    if (result.error)
+        throw result
+
+    return result
+}
+
+export const GET = handleNextRouteAsync(
+    async (req) => {
+        const code = req.nextUrl.searchParams.get('code') as string
+        const state = req.nextUrl.searchParams.get('state') as string
+
+        const { provider, returnTo } = decodeState(state)
+
+        const url = new URL(returnTo)
+
+        const token = await exchangeCodeForToken(provider, code)
+
+        const credential = await insert({ provider, ...token })
+
+        return returnRedirect(url.toString(), credential.id)
+    }
+)
