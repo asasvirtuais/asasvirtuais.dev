816006b4b2bd247c59441cf39806b89e02044179
Icaro Asas Virtuais <39817766+asasvirtuais@users.noreply.github.com>
2024-08-25 04:50:44 +0000

    + credentials table

---
+++ app/oauth/credentials/table.sql
@@ -0,0 +1,14 @@
+-- Then, create the table using this function
+CREATE TABLE credentials (
+    -- Headers
+    id VARCHAR(36) PRIMARY KEY,
+    provider VARCHAR(255) NOT NULL,
+    -- TokenData
+    access_token TEXT NOT NULL,
+    expires_in INT NOT NULL,
+    refresh_token TEXT NOT NULL,
+    token_type VARCHAR(255) NOT NULL,
+    scope TEXT NOT NULL,
+    -- Metadata
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
