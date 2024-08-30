2008943810245997f41294bffff92246efa0f7d3
Icaro Asas Virtuais <39817766+asasvirtuais@users.noreply.github.com>
2024-08-30 15:38:53 +0000

    avoid width variations

--- app/components/gallery-slider-carousel-flicker/index.tsx
+++ app/components/gallery-slider-carousel-flicker/index.tsx
@@ -4,6 +4,7 @@ import '@egjs/react-flicking/dist/flicking.css'
 import Flicking from '@egjs/react-flicking'
 import Panel from './panel'
 import { useState } from 'react'
+import { Box } from '@chakra-ui/react'
 
 export type GalleryItem = {
     title?: string
@@ -18,20 +19,22 @@ type Props = {
 export default function GallerySliderCarouselFlicker({ items = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}] }: Props) {
     const [open, setOpen] = useState<number>()
     return (
-        <Flicking
-            bound
-            align='prev'
-            onSelect={e => {
-                setOpen(e.panel.index)
-                e.panel.focus(600).then(() => e.currentTarget.resize())
-            }}
-            preventClickOnDrag
-            preventEventsBeforeInit
-            moveType='strict'
-        >
-            {items.map((item, i) => (
-                <Panel key={i} open={i === open} {...item} />
-            ))}
-        </Flicking>
+        <Box sx={{ '.flicking-camera': { px: 1, gap: 2 } }} >
+            <Flicking
+                bound
+                align='prev'
+                onSelect={e => {
+                    e.panel.focus(600)
+                        .then(() => e.currentTarget.resize())
+                }}
+                preventClickOnDrag
+                preventEventsBeforeInit
+                moveType='strict'
+            >
+                {items.map((item, i) => (
+                    <Panel key={i} onMouseUp={() => setOpen(i)} open={i === open} {...item} />
+                ))}
+            </Flicking>
+        </Box>
     )
 }
